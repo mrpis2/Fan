a27eecd642d053bf948729e325111e903566d83c
mrpis2 <warhammerprogamer@gmail.com>
2024-01-11 21:25:03 +0700

    fan

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Fan fan = new Fan(2, false ,5, "blue" );
+        Console.WriteLine(fan.ToString());
+    }
+}
+
+public class Fan
+{
+    protected int Speed { get; set; }
+    protected bool Turned { get; set; }
+    private double Radius { get; set; }
+    private string Color { get; set; }
+    public Fan(int speed, bool turned, double radius, string color)
+    {
+        this.Speed = speed;
+        this.Turned = turned;
+        this.Radius = radius;
+        this.Color = color;
+    }
+ 
+
+    public string ToString()
+    {
+        return "speed :" + this.Speed + '\n' + (this.Turned ? "turned" : "turn off") + '\n' + "radius: " + this.Radius + '\n' + "color: " + this.Color;
+    }
+}
